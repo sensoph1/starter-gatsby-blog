af3993b27527fb42f65dc80611bf33c8c60f040e
sensoph1 <sensoph@gmail.com>
2020-10-01 15:12:07 -0500

    css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,7 @@ class RootIndex extends React.Component {
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
+
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
